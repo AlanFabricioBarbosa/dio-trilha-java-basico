869fe98fe269370a0fe05ede2a50981123af7138
Alan Fabrício <alanfabriciobarbosa1@gmail.com>
2025-02-09 16:42:13 -0300

    :tada: Commit inicial

---
+++ src/edu/alan/javabasico/BoletimEstudantil.java
@@ -0,0 +1,14 @@
+package edu.alan.javabasico;
+
+public class BoletimEstudantil {
+    public static void main(String[] args){
+        int mediaFinal = 8;
+
+        if(mediaFinal < 6)
+            System.out.println("Reprovado");
+        else if (mediaFinal ==6)
+            System.out.println("Prova de Minerva");
+        else
+            System.out.println("Aprovado");
+    }
+}
